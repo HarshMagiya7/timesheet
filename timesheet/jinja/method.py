d90b214f443790a997d43fbf8b9c75ea1fb805d8
HarshMagiya7 <harshmagiya07@gmail.com>
2023-02-14 18:34:20 +0000

    Adding extra fields

--- timesheet/jinja/method.py
+++ timesheet/jinja/method.py
@@ -1,5 +1,24 @@
 import frappe
+
+# Remeber to import required modules
+from frappe.utils import get_url
+from frappe.utils.verified_command import get_signed_params
+
 @frappe.whitelist()
-def summ(a,b):
-	c = a + b
-	return c
+def get_workflow_action_url(action, user, pdanum,current_state):
+	apply_action_method = (
+		"/api/method/frappe.workflow.doctype.workflow_action.workflow_action.apply_action"
+	)
+
+	params = {
+
+		"doctype": 'Work Timesheet', # hard-coded in my case, you can pass it as an argument if you prefer
+		"docname": pdanum, # an argument in my case, you can hard-code it if you prefer
+		"action": action, # an argument in my case, you can hard-code it if you prefer
+		"current_state": current_state, # an argument in my case, you can hard-code it if you prefer
+		"user": user # an argument in my case, you can hard-code it if you prefer
+	}
+    # this will return you an URL to use in the href of the e-mail button
+	return get_url(apply_action_method + "?" + get_signed_params(params))
+
+
--- timesheet/timesheet/doctype/work_timesheet/work_timesheet.py
+++ timesheet/timesheet/doctype/work_timesheet/work_timesheet.py
@@ -3,11 +3,28 @@
 
 import frappe
 from frappe.model.document import Document
-
+from datetime import datetime
 class WorkTimesheet(Document):
-	pass
+	def validate(self):
+		self.check_date()
+		self.calculate_total_hours()
 
-@frappe.whitelist()
-def demo(a,b):
-	c = a+b
-	return c
+	def check_date(self):
+#		print(self.start_date)
+#		print(type(self.start_date))
+		if frappe.utils.get_datetime(self.start_date).weekday() != 0:
+			frappe.throw('Start date must be a monday')
+		start_date = frappe.utils.get_datetime(self.start_date)
+		end_date = frappe.utils.add_days(start_date,6)
+		self.end_date = frappe.utils.get_date_str(end_date)
+
+	def calculate_total_hours(self):
+
+		hours = 0
+
+		for i in self.timesheet:
+			week = [i.mon,i.tue,i.wed, i.thus, i.fri, i.sat, i.sun]
+			for j in week:
+				if j != None:
+					hours += j
+		self.total_working_hours = hours
